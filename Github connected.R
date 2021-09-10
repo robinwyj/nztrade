4f3cbceb27cbb633e5828d4b00e1754ad80f9510
louisli0813 <89519071+louisli0813@users.noreply.github.com>
2021-09-10 21:39:28 +1200

    merged data named MASTERSHEET

--- Github connected.R
+++ Github connected.R
@@ -48,6 +48,59 @@ colnames(EP20)[1] <- "Electorate"
  finaldf<-merge (votechange, df1, by="Electorate")
  
  
+ 
+ library(readr)
+ 
+ e2014 <- read_csv("~/Downloads/e9_part6.csv", 
+                      skip = 1)
+ e2008 <- read_csv("~/Downloads/e9_part6-2.csv", 
+                        skip = 1)
+ vote2014<- e2014[,c(1,3,6)]
+ vote2008<- e2008[,c(1,3,6)]
+ colnames(vote2014)[1]<-"Electorate"
+ colnames(vote2014)[2]<-"2014"
+ colnames(vote2014)[3]<-"2014 percentage"
+ colnames(vote2008)[1]<-"Electorate"
+ colnames(vote2008)[2]<-"2008"
+ colnames(vote2008)[3]<-"2008 percentage"
+ votechange<- merge (vote2008, vote2014, by="Electorate")
+ votechange$incumbent <- votechange$"2014" == votechange$"2008"
+ 
+ winning_electorate_candidates <- read_csv("~/Downloads/winning-electorate-candidates.csv", 
+                                           skip = 1)
+ vote2020<-(winning_electorate_candidates)
+ 
+ vote2020<- vote2020[,c(1,3,6)]
+ 
+ 
+ winning_electorate_candidates_2 <- read_csv("~/Downloads/winning-electorate-candidates-2.csv", 
+                                             skip = 1)
+ vote2017 <- winning_electorate_candidates_2
+ vote2017 <- vote2017[,c(1,3,6)]
+ 
+ colnames(vote2017)[1]<-"Electorate"
+ colnames(vote2017)[2]<-"2017"
+ colnames(vote2017)[3]<-"2017 percentage"
+ colnames(vote2020)[1]<-"Electorate"
+ colnames(vote2020)[2]<-"2020"
+ colnames(vote2020)[3]<-"2020 percentage"
+ votechange1 <- merge (vote2014, vote2017, by="Electorate")
+ votechange1$incumbent <- votechange1$"2014" == votechange1$"2017"
+ votechange2 <- merge (vote2017, vote2020, by="Electorate")
+ votechange2$incumbent <- votechange2$"2017" == votechange2$"2020"
+ 
+ votefirst <- merge (votechange, votechange1, by="Electorate")
+ votesecond <- merge (votefirst, votechange2, by="Electorate")
+ FINALVOTE <- votesecond[,c(1:6,9:11,14:16)]
+ colnames(FINALVOTE)[6]<- "Incumbent2008-2014"
+   colnames(FINALVOTE)[9]<- "Incumbent2014-2017"
+   colnames(FINALVOTE)[12]<- "Incumbent2017-2020"
+  write.csv(FINALVOTE, "Votechange 2008-2020")
+   
+ Trend<-read.csv("Votechange 2008-2020")
+ Trend <- Trend[,2:13]
+ MASTERSHEET <- merge(finaldf, Trend, by="Electorate")
+ 
  #Below are codes comiling data and writing csv.
  library(readr)
  winning_electorate_candidates <- read_csv("~/Downloads/winning-electorate-candidates.csv", 
