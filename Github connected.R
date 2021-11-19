82222ba63e40b3cb13698a5efd50ba68d8c1dbd0
louisli0813 <89519071+louisli0813@users.noreply.github.com>
2021-11-20 11:25:29 +1300

    Trade update
    Trade shock changes now finished for 95,00,07

--- Github connected.R
+++ Github connected.R
@@ -138,3 +138,106 @@ colnames(EP20)[1] <- "Electorate"
  rawEP20<- electorate_profiles_2020_data_file
  
  write.csv (rawEP20, "ep20.csv")
+ 
+ EP08_list <- split(EP08, seq(nrow(EP08)))
+ EP08_list
+ 
+ list(MASTERSHEET$Electorate)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ # Test
+ #load datasets. We are using three years but there are more. 
+ icio_07 <- read.csv("~/Documents/ICIO2016_2007.csv")
+ icio_00 <- read.csv("~/Documents/ICIO2016_2000.csv")
+ icio_95 <- read.csv("ICIO2016_1995.csv")
+ 
+ 
+ all_cty <- unique(substr(colnames(icio_07), 1, 3))[-1]
+ all_cty <- all_cty[-c(72:length(all_cty))]
+ all_cty <- all_cty[!all_cty== "NZL"]
+ 
+ #two matrice for 1995
+ icio_95_chn_cn2 <- data.frame(icio_95[substr(icio_95$X, 1, 3) %in% c ("CN2"), 
+                                       substr(colnames(icio_95), 1, 3) == "NZL"])
+ 
+ icio_95_chn_cn3 <- data.frame(icio_95[substr(icio_95$X, 1, 3) %in% c ("CN3"), 
+                                       substr(colnames(icio_95), 1, 3) == "NZL"])
+ 
+ 
+ 
+ #two matrice for 2000
+ icio_00_chn_cn2 <- data.frame(icio_00[substr(icio_00$X, 1, 3) %in% c ("CN2"), 
+                                       substr(colnames(icio_00), 1, 3) == "NZL"])
+ 
+ icio_00_chn_cn3 <- data.frame(icio_00[substr(icio_00$X, 1, 3) %in% c ("CN3"), 
+                                       substr(colnames(icio_00), 1, 3) == "NZL"])
+ 
+ #two matrice for 2007
+ icio_07_chn_cn2 <- data.frame(icio_07[substr(icio_07$X, 1, 3) %in% c ("CN2"), 
+                                       substr(colnames(icio_07), 1, 3) == "NZL"])
+ 
+ icio_07_chn_cn3 <- data.frame(icio_07[substr(icio_07$X, 1, 3) %in% c ("CN3"), 
+                                       substr(colnames(icio_07), 1, 3) == "NZL"])
+ 
+ 
+ #two matrice for 1995
+ #generate cn2 
+ icio_95_chn_cn2 <- data.frame(icio_95[substr(icio_95$X, 1, 3) %in% c ("CN2"), 
+                                       substr(colnames(icio_95), 1, 3) == "NZL"])
+ 
+#sum up each row of cn2 for Chinese export to NZL for each industry 
+icio_95_chn_cn2_sum <- rowSums(icio_95_chn_cn2)
+#generate cn3
+icio_95_chn_cn3 <- data.frame(icio_95[substr(icio_95$X, 1, 3) %in% c ("CN3"), 
+                                      substr(colnames(icio_95), 1, 3) == "NZL"])
+#same for cn3
+icio_95_chn_cn3_sum <- rowSums(icio_95_chn_cn3)
+#sum up cn2 and cn3 
+chn_nzl_exp_95 <- icio_95_chn_cn2_sum + icio_95_chn_cn3_sum
+#create a dataset `chn_nzl_exp_95`. It has two columns: the first column is the name of each Chinese industry; the second column is the corresponding Chinese exports to NZL 
+chn_nzl_exp_95 <- data.frame(industry = substr(colnames(icio_95_chn_cn2), 
+                                               5, nchar(colnames(icio_95_chn_cn2))), 
+                             exp_val = chn_nzl_exp_95)
+
+
+
+icio_00_chn_cn2 <- data.frame(icio_00[substr(icio_00$X, 1, 3) %in% c ("CN2"), 
+                                      substr(colnames(icio_00), 1, 3) == "NZL"])
+
+icio_00_chn_cn2_sum <- rowSums(icio_00_chn_cn2)
+
+icio_00_chn_cn3 <- data.frame(icio_00[substr(icio_00$X, 1, 3) %in% c ("CN3"), 
+                                      substr(colnames(icio_00), 1, 3) == "NZL"])
+icio_00_chn_cn3_sum <- rowSums(icio_00_chn_cn3)
+chn_nzl_exp_00 <- icio_00_chn_cn2_sum + icio_00_chn_cn3_sum
+chn_nzl_exp_00 <- data.frame(industry = substr(colnames(icio_00_chn_cn2), 
+                                               5, nchar(colnames(icio_00_chn_cn2))), 
+                             exp_val = chn_nzl_exp_00)
+
+
+
+
+icio_07_chn_cn2 <- data.frame(icio_07[substr(icio_07$X, 1, 3) %in% c ("CN2"), 
+                                      substr(colnames(icio_07), 1, 3) == "NZL"])
+
+icio_07_chn_cn2_sum <- rowSums(icio_07_chn_cn2)
+
+icio_07_chn_cn3 <- data.frame(icio_07[substr(icio_07$X, 1, 3) %in% c ("CN3"), 
+                                      substr(colnames(icio_07), 1, 3) == "NZL"])
+icio_07_chn_cn3_sum <- rowSums(icio_07_chn_cn3)
+chn_nzl_exp_07 <- icio_07_chn_cn2_sum + icio_07_chn_cn3_sum
+chn_nzl_exp_07 <- data.frame(industry = substr(colnames(icio_07_chn_cn2), 
+                                               5, nchar(colnames(icio_07_chn_cn2))), 
+                             exp_val = chn_nzl_exp_07)
+
+
+"95+00" <- merge (chn_nzl_exp_95, chn_nzl_exp_00, by="industry")
+
